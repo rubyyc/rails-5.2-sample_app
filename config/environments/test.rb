6c2f8b23d9670714a1ebc3bf772cf10eadab0ade
YuanChang <759337828@qq.com>
2022-05-24 13:31:47 +0800

    config rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+$VERBOSE=nil
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -35,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  config.active_support.deprecation = :log
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
